5eed0e17af244ba8e623874cd3f0734eb10ac839
Carol Fantinate <carolfantinate@outlook.com>
2024-10-05 18:48:14 -0300

    exercicios js

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var v = 50
+console.log(`A velocidade do seu carro é de ${v}km/h`)
+
+if (v < 60) {
+    console.log('Dirija sempre usando o cinto de segurança!')
+} else {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+
